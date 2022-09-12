fa3743b53cf4135906b0c3d4544869e7c8506e19
ElieBadari <elie.badari@hotmail.com>
2022-09-12 13:27:58 +0300

    implemented visibility toggling for the images

--- CSC498X_CoursePractice/TicTacToeApp/app/src/main/java/com/elieb/tictactoe/MainActivity.java
+++ CSC498X_CoursePractice/TicTacToeApp/app/src/main/java/com/elieb/tictactoe/MainActivity.java
@@ -3,12 +3,34 @@ package com.elieb.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean is_happy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        is_happy = true;
+
     }
+
+    public void toggle(View v){
+        ImageView happy = (ImageView) findViewById(R.id.happy);
+        ImageView sad = (ImageView) findViewById(R.id.sad);
+
+        if(is_happy){
+            happy.setVisibility(View.INVISIBLE);
+            sad.setVisibility(View.VISIBLE);
+            is_happy = false;
+        }
+        else{
+            happy.setVisibility(View.VISIBLE);
+            sad.setVisibility(View.INVISIBLE);
+            is_happy = true;
+        }
+    }
+
 }
