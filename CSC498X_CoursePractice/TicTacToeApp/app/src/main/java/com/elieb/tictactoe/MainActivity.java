e55c87cc5a31c942b81d5ffa60e26e9cdbf3eff6
ElieBadari <elie.badari@hotmail.com>
2022-09-12 13:32:43 +0300

    implemented toggling using animate funct

--- CSC498X_CoursePractice/TicTacToeApp/app/src/main/java/com/elieb/tictactoe/MainActivity.java
+++ CSC498X_CoursePractice/TicTacToeApp/app/src/main/java/com/elieb/tictactoe/MainActivity.java
@@ -9,26 +9,35 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     boolean is_happy;
+    ImageView happy;
+    ImageView sad;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         is_happy = true;
+        happy = (ImageView) findViewById(R.id.happy);
+        sad = (ImageView) findViewById(R.id.sad);
 
     }
 
     public void toggle(View v){
-        ImageView happy = (ImageView) findViewById(R.id.happy);
-        ImageView sad = (ImageView) findViewById(R.id.sad);
+        //ImageView happy = (ImageView) findViewById(R.id.happy);
+        //ImageView sad = (ImageView) findViewById(R.id.sad);
 
         if(is_happy){
-            happy.setVisibility(View.INVISIBLE);
-            sad.setVisibility(View.VISIBLE);
+//            happy.setVisibility(View.INVISIBLE);
+//            sad.setVisibility(View.VISIBLE);
+            happy.animate().alpha(0).setDuration(2000);
+            sad.animate().alpha(1).setDuration(2000);
             is_happy = false;
         }
         else{
-            happy.setVisibility(View.VISIBLE);
-            sad.setVisibility(View.INVISIBLE);
+//            happy.setVisibility(View.VISIBLE);
+//            sad.setVisibility(View.INVISIBLE);
+            happy.animate().alpha(1).setDuration(2000);
+            sad.animate().alpha(0).setDuration(2000);
             is_happy = true;
         }
     }
