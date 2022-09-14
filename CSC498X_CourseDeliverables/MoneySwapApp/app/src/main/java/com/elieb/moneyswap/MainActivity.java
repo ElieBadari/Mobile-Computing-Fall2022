73d44758a42dc55a1f2e7794b98300aeae69bd09
ElieBadari <elie.badari@hotmail.com>
2022-09-14 11:27:21 +0300

    extracted username and password from user input

--- CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity.java
+++ CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity.java
@@ -1,9 +1,9 @@
 package com.elieb.moneyswap;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void logIn(View v){
 
-        
+        EditText user = findViewById(R.id.username);
+        EditText pass = findViewById(R.id.password);
+
+
+
     }
 
 }
