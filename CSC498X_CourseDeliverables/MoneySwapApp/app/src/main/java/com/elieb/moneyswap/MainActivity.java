2576571dfec517d8083479caa3eca83842c08978
ElieBadari <elie.badari@hotmail.com>
2022-09-14 11:20:38 +0300

    added eventlistner for login button

--- CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity.java
+++ CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity.java
@@ -3,12 +3,21 @@ package com.elieb.moneyswap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void logIn(View v){
+
+        
+    }
+
 }
