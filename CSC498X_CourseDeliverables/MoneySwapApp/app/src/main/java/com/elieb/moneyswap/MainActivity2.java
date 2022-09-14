783eba92ca93285456e4fd39b0571812bce802f3
ElieBadari <elie.badari@hotmail.com>
2022-09-14 23:12:20 +0300

    added functionality for radio button

--- CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity2.java
+++ CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity2.java
@@ -3,7 +3,10 @@ package com.elieb.moneyswap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.FloatProperty;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,7 +20,27 @@ public class MainActivity2 extends AppCompatActivity {
 
     public void swapMoney(View v){
 
-        
+        TextView monies = (TextView) findViewById(R.id.money);
+        float money = Float.parseFloat(monies.getText().toString());
+        TextView amount_to_disp = (TextView) findViewById(R.id.amount);
+
+        Switch choice = (Switch) findViewById(R.id.radio1);
+
+        choice.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (choice.isChecked()){
+                    amount_to_disp.setText("The amount in USD = " + money/40000);
+
+                }else{
+                    amount_to_disp.setText("The amount in LBP = " + money*40000);
+                }
+            }
+        });
+
+
+
+
 
 
 
