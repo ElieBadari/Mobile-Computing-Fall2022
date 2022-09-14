56a6e0895ebbd058bee9dd7298bb52dae58ac217
ElieBadari <elie.badari@hotmail.com>
2022-09-14 22:50:34 +0300

    added radio btn and more UI updates

--- CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity2.java
+++ CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity2.java
@@ -3,6 +3,9 @@ package com.elieb.moneyswap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+
+    public void swapMoney(View v){
+
+        
+
+
+
+    }
+
 }
