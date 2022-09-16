c2d2bc81bf4fe5e45b407eefb8829206f7ab5932
ElieBadari <elie.badari@hotmail.com>
2022-09-16 21:03:43 +0300

    fixed swap button making it functional

--- CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity2.java
+++ CSC498X_CourseDeliverables/MoneySwapApp/app/src/main/java/com/elieb/moneyswap/MainActivity2.java
@@ -14,10 +14,13 @@ import java.text.DecimalFormat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Switch choice;
+    String to_display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        choice = (Switch) findViewById(R.id.radio1);
     }
 
     public void swapMoney(View v){
@@ -26,28 +29,25 @@ public class MainActivity2 extends AppCompatActivity {
         float money = Float.parseFloat(monies.getText().toString());
         TextView amount_to_disp = (TextView) findViewById(R.id.amount);
 
-        Switch choice = (Switch) findViewById(R.id.radio1);
-
         choice.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (choice.isChecked()){
 
-                    amount_to_disp.setText("The amount in USD = " + money/40000);
+                    to_display = "The amount in USD" + money/40000;
+//                    amount_to_disp.setText("The amount in USD = " + money/40000 +" $");
 
                 }else{
                     float to_disp = money*40000 ;
                     DecimalFormat df = new DecimalFormat("#");
                     df.setMaximumFractionDigits(0);
-                    amount_to_disp.setText("The amount in LBP = " + df.format(to_disp));
+                    to_display = "The amount in LBP = " + df.format(to_disp) + "l.l";
+//                    amount_to_disp.setText("The amount in LBP = " + df.format(to_disp) + "l.l");
                 }
             }
+
         });
-
-
-
-
-
+        amount_to_disp.setText(to_display);
 
 
     }
