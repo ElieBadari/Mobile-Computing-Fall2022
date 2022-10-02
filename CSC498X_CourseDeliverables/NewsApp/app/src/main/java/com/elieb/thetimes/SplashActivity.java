85e7051c9f324b51f2f514b1577db9f7381221c2
ElieBadari <elie.badari@hotmail.com>
2022-10-03 00:16:28 +0300

    delayed the intent to sync w/ the splash screen

--- CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/SplashActivity.java
+++ CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/SplashActivity.java
@@ -2,13 +2,32 @@ package com.elieb.thetimes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        icon = (ImageView) findViewById(R.id.icon);
+        icon.animate().rotationY(360).setDuration(3000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        }, 3000);
+
     }
 }
