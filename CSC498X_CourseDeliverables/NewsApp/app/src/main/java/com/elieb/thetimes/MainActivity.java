17fb95624462f975669ab9d1fe8b13d3cf690c8c
ElieBadari <elie.badari@hotmail.com>
2022-10-03 01:34:27 +0300

    added some animation

--- CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/MainActivity.java
+++ CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/MainActivity.java
@@ -3,17 +3,24 @@ package com.elieb.thetimes;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+
 import android.os.Bundle;
+
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView icon;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        icon = (ImageView) findViewById(R.id.icon);
+        icon.animate().rotationY(360);
 
 
 
