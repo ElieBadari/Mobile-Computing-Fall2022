5cc81f69937ca2d876084d791dc425da77feceb5
ElieBadari <elie.badari@hotmail.com>
2022-10-03 01:31:47 +0300

    added UI for 2nd page and text input

--- CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/MainActivity.java
+++ CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/MainActivity.java
@@ -2,13 +2,24 @@ package com.elieb.thetimes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
     }
 }
