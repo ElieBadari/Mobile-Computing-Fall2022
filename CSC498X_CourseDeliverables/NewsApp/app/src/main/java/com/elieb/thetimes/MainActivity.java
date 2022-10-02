52694f48daf32ecb8b1cf674f257c61099c3532a
ElieBadari <elie.badari@hotmail.com>
2022-10-03 02:50:16 +0300

    added sharedpref + new page and its linking

--- CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/MainActivity.java
+++ CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
     EditText name;
     String user;
     Button btn;
-
-
+    SharedPreferences sp;
+    String name_str;
 
 
     @Override
@@ -29,10 +33,30 @@ public class MainActivity extends AppCompatActivity {
 
         icon = (ImageView) findViewById(R.id.icon);
         icon.animate().rotationY(360);
+        sp = getSharedPreferences("com.elieb.theTimes",Context.MODE_PRIVATE);
 
         name = (EditText) findViewById(R.id.name);
         user = name.getText().toString();
         btn = (Button) findViewById(R.id.save);
+
+
+    }
+    public void onClick(View view){
+
+        name_str = name.getText().toString();
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("com.elieb.theTime.name",name_str);
+        editor.commit();
+        Toast.makeText(getApplicationContext(), "Information Saved, Welcome!", Toast.LENGTH_LONG).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(getApplicationContext(), NewsActivity.class);
+                startActivity(i);
+            }
+        }, 1000);
 
     }
 
---
+++ CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/NewsActivity.java
@@ -0,0 +1,18 @@
+package com.elieb.thetimes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+    }
+
+
+}
