f51cc058a7ef65b21d35ff048a013d5b05742069
ElieBadari <elie.badari@hotmail.com>
2022-10-03 17:33:08 +0300

    extracted article names from db

--- CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/NewsActivity.java
+++ CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/NewsActivity.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class NewsActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> array_adapter;
+    ArrayList<String> article_list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,16 @@ public class NewsActivity extends AppCompatActivity {
 
             sql.execSQL("INSERT INTO articles (article_name, author , published_at , location , description ) VALUES ('I Make Video Games. I Won’t Let My Daughters Play Them.','William Siu','Oct. 2, 2022','https://www.nytimes.com/2022/10/02/opinion/video-game-addiction.html','I didnt see a problem for a long time. I saw our mission as bringing players joy and entertainment. Knowing all of the techniques we used to induce addiction, I realized I didnt want my children to be exposed to that risk. \n')");
             sql.execSQL("INSERT INTO articles (article_name, author , published_at , location , description ) VALUES ('Kim Kardashian to Pay $1.26 Million to Settle S.E.C. Charges Over Crypto Promotion','Matthew Goldstein','Oct. 3, 2022','https://www.nytimes.com/2022/10/02/opinion/video-game-addiction.html','The S.E.C. said Ms. Kardashian did not disclose she had been paid to promote a crypto token sold by EthereumMax.')");
+
+            Cursor c = sql.rawQuery("Select * from articles", null);
+            int article_name_index = c.getColumnIndex("article_name");
+            c.moveToFirst();
+
+            while(c!= null){
+                String name = c.getString(article_name_index);
+                article_list.add(name);
+
+            }
 
         }catch(Exception e){
             e.printStackTrace();
