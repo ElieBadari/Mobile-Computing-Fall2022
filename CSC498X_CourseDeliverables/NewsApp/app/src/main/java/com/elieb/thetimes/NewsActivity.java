b2cdae9b4243295af4084d19806a2cb6efdc4c8d
ElieBadari <elie.badari@hotmail.com>
2022-10-03 17:28:17 +0300

    Added 2 articles

--- CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/NewsActivity.java
+++ CSC498X_CourseDeliverables/NewsApp/app/src/main/java/com/elieb/thetimes/NewsActivity.java
@@ -18,6 +18,8 @@ public class NewsActivity extends AppCompatActivity {
             SQLiteDatabase sql = this.openOrCreateDatabase("thetimesdb", MODE_PRIVATE, null);
             sql.execSQL("CREATE TABLE IF NOT EXISTS articles (article_name VARCHAR, author VARCHAR, published_at VARCHAR, location VARCHAR, description VARCHAR)");//author, published at, location, description
 
+            sql.execSQL("INSERT INTO articles (article_name, author , published_at , location , description ) VALUES ('I Make Video Games. I Won’t Let My Daughters Play Them.','William Siu','Oct. 2, 2022','https://www.nytimes.com/2022/10/02/opinion/video-game-addiction.html','I didnt see a problem for a long time. I saw our mission as bringing players joy and entertainment. Knowing all of the techniques we used to induce addiction, I realized I didnt want my children to be exposed to that risk. \n')");
+            sql.execSQL("INSERT INTO articles (article_name, author , published_at , location , description ) VALUES ('Kim Kardashian to Pay $1.26 Million to Settle S.E.C. Charges Over Crypto Promotion','Matthew Goldstein','Oct. 3, 2022','https://www.nytimes.com/2022/10/02/opinion/video-game-addiction.html','The S.E.C. said Ms. Kardashian did not disclose she had been paid to promote a crypto token sold by EthereumMax.')");
 
         }catch(Exception e){
             e.printStackTrace();
