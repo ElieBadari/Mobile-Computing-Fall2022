b63fa39045d8fde74631d2a49dad15985d501912
ElieBadari <elie.badari@hotmail.com>
2022-11-29 11:09:04 +0200

    added another page and linked it

--- CSC498X_CourseDeliverables/prediction-app/src/app/home/home-routing.module.ts
+++ CSC498X_CourseDeliverables/prediction-app/src/app/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PredictionPage } from '../prediction/prediction.page';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path:'prediction',
+    component: PredictionPage
   }
 ];
 
