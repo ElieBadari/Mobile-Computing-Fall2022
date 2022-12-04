3f2c25a9d7d096b676872abe0b21426b2a4fd5ef
ElieBadari <elie.badari@hotmail.com>
2022-12-04 19:14:30 +0200

    linked dogimg api to frontend

--- CSC498X_CourseDeliverables/prediction-app/src/app/home/home.page.ts
+++ CSC498X_CourseDeliverables/prediction-app/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApisService } from '../apis/apis.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  image : string = '';
+ 
+  constructor(private apiService:ApisService, private router:Router) {}
 
+    ngOnInit(){ 
+      this.apiService.getImage().subscribe((response: any) => {
+        this.image = response.message;
+    });
+
+  }
+    anodaOne(){
+      this.apiService.getImage().subscribe((response: any) => {
+        this.image = response.message;
+
+    });
+  }
 }
+
+
